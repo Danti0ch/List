8bdcfee4d8a29d18d3ddfc8c2a0eea5e972e2cd8
Danti0ch <89589647+Danti0ch@users.noreply.github.com>
2022-04-08 14:20:07 +0300

    Update list.cpp

--- src/list.cpp
+++ src/list.cpp
@@ -3,104 +3,523 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <sys\stat.h> 
 
-verif_code list_verification(list* obj);
+#define list_resize(obj, mode) _list_resize((obj), (mode), #obj, LOCATION)
 
-void list_dump(list* obj, meta_info *meta, char const * file_name, char const * func_name, int const n_line);
+#define  get_ind_of_free(obj) _get_ind_of_free((obj), #obj, LOCATION)
 
-void dump_list_data(list* obj);
+ERR_CODE _list_resize(list* obj, int mode, META_PARAMS);
+
+int _get_ind_of_free(list* obj, META_PARAMS);
+
+int node_cmp(const void * node1, const void * node2);
+
+VERIF_CODE list_verification(list* obj);
+
+void list_dump(list* obj, meta_info *meta, META_PARAMS);
+
+void dump_nodes(list* obj);
 
 void print_list(list* obj);
 
-void _ListConstructor(list* obj, size_t const capacity, const char* name, META_PARAMS){
+int get_file_size(const char* name);
+
+//---------------------------------------------------------------------------------------//
+
+ERR_CODE _ListConstructor(list* obj, const int capacity, META_PARAMS){
 
 	assert(obj != NULL);
 
 	if(capacity > 0){
 		obj->nodes = (node*)calloc(capacity, sizeof(node));
-		assert(obj->nodes != NULL);
+		
+		if(obj->nodes == NULL){
+			return ERR_CODE::MEM_ALLOC;
+		}
 	}
 
-	for(int n_elem = 1; n_elem < capacity; n_elem++){
-		obj->nodes[n_elem].next = n_elem + 1;
-		obj->nodes[n_elem].prev = PREV_INVALID_VAL;
+	for(int n_node = 1; n_node < capacity; n_node++){
+		obj->nodes[n_node].next = n_node + 1;
+		obj->nodes[n_node].prev = FREE_INDICATION_VAL;
 	}
 
-	obj->is_sorted = VAL_SORTED;
+	obj->nodes[capacity - 1].next = 0;
 
-	obj->head = 0;
-	obj->tail = 1;
-	obj->size = 0;
-	obj->capacity = capacity;
-
-	META_INIT(obj->meta.birth)
-
-	obj->meta.name = (char*)calloc(strlen(name), sizeof(char));
-	strcpy(obj->meta.name, name);
-
-	obj->meta.death.file_name = NULL;
-	obj->meta.death.func_name = NULL;
-	obj->meta.death.n_line    = -1;
+	obj->is_sorted 	= VAL_SORTED;
+	
+	obj->head 		= 0;
+	obj->tail 		= 0;
+	
+	obj->head_free 	= 1;
+	obj->tail_free 	= capacity - 1;
+	
+	obj->size 		= 0;
+	obj->capacity 	= capacity;
 
 	LIST_OK(obj)
 
-	return;
+	return ERR_CODE::OK;
 }
 
-void _ListDestructor(list* obj, META_PARAMS){
+ERR_CODE _ListDestructor(list* obj, META_PARAMS){
 
+	assert(obj != NULL);
 	LIST_OK(obj)
 
 	free(obj->nodes);
 	obj->nodes = (node*)FREE_MEM_POINTER_VAL;
 
-	// DESTRUCTION META INFO CREATION
-	META_INIT(obj->meta.death)
+	obj->head 		= -1;
+	obj->tail 		= -1;
+	obj->head_free  = -1;
+	obj->capacity 	= -1;
+	obj->size 		= -1;
+
+	return ERR_CODE::OK;
+}
+
+node* _ListFront(list* obj, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	if(obj->size > 0) return &(obj->nodes[obj->head]);
+	else return NULL;
+}
+
+node* _ListBack(list* obj, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	if(obj->size > 0) return &(obj->nodes[obj->tail]);
+	else return NULL;
+}
+
+node* _ListAfter(list* obj, node* nod, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	if(nod != ListBack(obj)) return &(obj->nodes[nod->next]);
+	else return NULL;
+}
+
+node* _ListBefore(list* obj, node* nod, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+	
+	if(nod != ListFront(obj)) return &(obj->nodes[nod->prev]);
+	else return NULL;
+}
+
+ERR_CODE _PushFront(list* obj, list_T val, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	if(obj->size + 2 >= obj->capacity){
+		int resize_rez = (int)list_resize(obj, INCREASE_MODE);
+		
+		if(resize_rez != (int)ERR_CODE::OK){
+			return (ERR_CODE)resize_rez;
+		}
+	}
+
+	int new_head_pos = get_ind_of_free(obj);
+
+	obj->nodes[new_head_pos].next = obj->head;
+	obj->nodes[new_head_pos].prev = 0;
+
+	if(obj->head != 0) obj->nodes[obj->head].prev = new_head_pos;
+
+	obj->head = new_head_pos;
+	obj->nodes[obj->head].val = val;
+
+	if(obj->size == 0)	obj->tail = new_head_pos;
+
+	obj->size++;
+
+	LIST_OK(obj)
+	return ERR_CODE::OK;
+}
+
+ERR_CODE _PushBack(list* obj, list_T val, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	if(obj->size + 2 >= obj->capacity){
+		int resize_rez = (int)list_resize(obj, INCREASE_MODE);
+		
+		if(resize_rez != (int)ERR_CODE::OK){
+			return (ERR_CODE)resize_rez;
+		}
+	}
+
+	int new_tail_pos = get_ind_of_free(obj);
+
+	obj->nodes[new_tail_pos].next = 0;
+	obj->nodes[new_tail_pos].prev = obj->tail;
+
+	if(obj->tail != 0) obj->nodes[obj->tail].next = new_tail_pos;
+
+	obj->tail = new_tail_pos;
+	obj->nodes[obj->tail].val = val;
+
+	if(obj->size == 0)	obj->head = new_tail_pos;
+
+	obj->size++;
+
+	LIST_OK(obj)
+
+	return ERR_CODE::OK;
+}
+
+ERR_CODE _ListInsert(list* obj, int pos, list_T val, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj);
+
+	if(pos < 0 || pos >= obj->size) return ERR_CODE::INVALID_POS;
+
+	if(pos == 0){
+		PushFront(obj, val);
+		return ERR_CODE::OK;
+	}
+	
+	if(pos == obj->size - 1){
+		PushBack(obj, val);
+		return ERR_CODE::OK;
+	}
+
+	if(obj->size + 2 >= obj->capacity){
+		int resize_rez = (int)list_resize(obj, INCREASE_MODE);
+		
+		if(resize_rez != (int)ERR_CODE::OK){
+			return (ERR_CODE)resize_rez;
+		}
+	}
+
+	int pos_of_prev = SearchByPos(obj, pos);
+
+	int new_node_pos = get_ind_of_free(obj);
+
+	obj->nodes[new_node_pos].next = obj->nodes[pos_of_prev].next;
+	obj->nodes[new_node_pos].prev = pos_of_prev;
+	obj->nodes[new_node_pos].val  = val;
+
+	obj->nodes[pos_of_prev].next 					= new_node_pos;
+	obj->nodes[obj->nodes[new_node_pos].next].prev 	= new_node_pos;
+	obj->size++;
+
+	LIST_OK(obj)
+	return ERR_CODE::OK;
+}
+
+ERR_CODE _ListRemove(list* obj, int pos, META_PARAMS){
+
+	assert(obj != NULL);
+
+	LIST_OK(obj);
+
+	if(pos < 0 || pos >= obj->size) return ERR_CODE::INVALID_POS;
+
+	if(obj->size * REDUCE_RATIO <= obj->capacity){
+		int resize_rez = (int)list_resize(obj, REDUCE_MODE);
+
+		if(resize_rez != (int)ERR_CODE::OK){
+			return (ERR_CODE)resize_rez;
+		}
+	}
+
+	dump_nodes(obj);
+	int physical_pos = SearchByPos(obj, pos);
+
+	int pos_next = obj->nodes[physical_pos].next;
+	int pos_prev = obj->nodes[physical_pos].prev;
+
+	if(pos != 0){
+		obj->nodes[pos_prev].next = pos_next;
+	}
+	else{
+		obj->head = pos_next;
+	}
+
+	if(pos != obj->size - 1){
+		obj->nodes[pos_next].prev = pos_prev;
+	}
+	else{
+		obj->tail = pos_prev;
+	}
+
+	obj->nodes[physical_pos].prev = -1;
+	obj->nodes[physical_pos].val  = POISON_VAL;
+	obj->nodes[physical_pos].next = 0;
+
+	if(obj->head_free == 0){
+		obj->head_free = physical_pos;
+		obj->tail_free = physical_pos;
+	}
+	else{
+		obj->nodes[obj->tail_free].next 	= physical_pos;
+		obj->tail_free 						= physical_pos;
+	}
+
+	obj->size--;
+
+	LIST_OK(obj)
+	return ERR_CODE::OK;
+}
+
+ERR_CODE _ListRemoveAll(list* obj, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	for(int n_node = 1; n_node < obj->capacity; n_node++){
+
+		obj->nodes[n_node].val  = POISON_VAL;
+		obj->nodes[n_node].prev = FREE_INDICATION_VAL;
+		obj->nodes[n_node].next = n_node + 1;
+	}
+
+	obj->nodes[obj->capacity - 1].next = 0;
+
+	obj->size = 0;
+	obj->head_free = 1;
+	obj->tail_free = obj->capacity - 1;
+
+	obj->head = 0;
+	obj->tail = 0;
+
+	LIST_OK(obj)
+	
+	return ERR_CODE::OK;
+}
+
+int _SearchByPos(list* obj, int pos, META_PARAMS){
+	
+	assert(obj != NULL);
+	LIST_OK(obj);
+
+	int counter = 0;
+
+	int n_node = obj->head;
+
+	while((counter < pos) && (n_node != 0)){
+		counter++;
+		n_node = obj->nodes[n_node].next;
+	}
+
+	return n_node;
+}
+
+int _get_ind_of_free(list* obj, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj);
+
+	int ind = obj->head_free;
+
+	if(obj->nodes[ind].next != 0){
+		int head_free_next = obj->nodes[ind].next;
+		obj->head_free = head_free_next;
+	}
+
+	return ind;
+}
+
+ERR_CODE _list_resize(list* obj, int mode, META_PARAMS){
+
+	assert(obj != NULL);
+	LIST_OK(obj)
+
+	if(mode == INCREASE_MODE){
+
+		int new_capacity = obj->capacity * INCREASE_RATIO;
+		
+		node* p_new_mem = (node*)calloc(new_capacity, sizeof(node));
+
+		if(p_new_mem == NULL){
+			return ERR_CODE::MEM_ALLOC;
+		}
+
+		memcpy(p_new_mem, obj->nodes, obj->capacity * sizeof(node));
+
+		free(obj->nodes);
+
+		obj->nodes    = p_new_mem;
+		p_new_mem     = NULL;
+
+		for(int n_node = obj->capacity; n_node < new_capacity ; n_node++){
+			
+			obj->nodes[n_node].next = n_node + 1;
+			obj->nodes[n_node].prev = FREE_INDICATION_VAL;
+		}
+		
+		if(obj->nodes[obj->capacity - 1].prev == FREE_INDICATION_VAL){
+			obj->nodes[obj->capacity - 1].next = obj->capacity;
+		}
+
+		if(obj->nodes[new_capacity - 1].prev == FREE_INDICATION_VAL){
+			obj->nodes[new_capacity - 1].next = 0;
+		}
+
+		obj->nodes[obj->tail_free].next = obj->capacity;
+		obj->tail_free 						= new_capacity - 1;
+
+		obj->capacity = new_capacity;
+
+	}
+
+	/*
+	else if(mode == REDUCE_MODE){
+		
+		SortList(obj);
+
+		int new_capacity = obj->capacity / REDUCE_RATIO;
+		
+		node* p_new_mem = (node*)calloc(new_capacity, sizeof(node));
+	
+		if(p_new_mem == NULL){
+			return ERR_CODE::MEM_ALLOC;
+		}
+
+		memcpy(p_new_mem, obj->nodes, new_capacity * sizeof(node));
+
+		free(obj->nodes);
+
+		obj->nodes = p_new_mem;
+		obj->capacity = new_capacity;
+		p_new_mem = NULL;
+
+		if(obj->nodes[new_capacity - 1].prev == FREE_INDICATION_VAL){
+			obj->nodes[new_capacity - 1].next = 0;
+		}
+	}
+	*/
+	LIST_OK(obj)
+	return ERR_CODE::OK;
+}
+
+
+int node_cmp(const void * node1, const void * node2){
+
+	assert(node1 != NULL);
+	assert(node2 != NULL);
+	assert(node1 != node2);
+
+	return (int)((uint32_t)(((node*)node1)->prev)) - (int)((uint32_t)(((node*)node2)->prev));
+}
+
+void nodes_swap(node* a, node* b){
+	
+	assert(a != NULL);
+	assert(b != NULL);
+	assert(a != b);
+
+	node c;
+
+	memcpy(&c, a, sizeof(node));
+	memcpy(a, b, sizeof(node));
+	memcpy(b, &c, sizeof(node));	
 
 	return;
 }
+/*
+void _SortList(list* obj, META_PARAMS){
 
-verif_code list_verification(list* obj){
+	assert(obj != NULL);
+
+	int *swap_arr = (int*)calloc(obj->capacity, sizeof(int));
+
+	swap_arr[0] = 0;
+
+	int counter = 1;
+
+	for(int n_node = obj->head; n_node != 0; n_node = obj->nodes[n_node].next, counter++){
+		swap_arr[counter] = n_node;
+	}
+
+	for(int n_node = 0; n_node < counter; n_node++){
+
+	}
+
+	qsort(obj->nodes + 1, obj->capacity, sizeof(node), node_cmp);
+
+	return;
+}
+*/
+
+VERIF_CODE list_verification(list* obj){
 
 	assert(obj != NULL);
 
 	if(obj->nodes == NULL){
-		return verif_code::corrupted_mem;
+		return VERIF_CODE::CORRUPTED_MEM;
+	}
+
+	if((obj->head < 0) || (obj->head >= obj->capacity)){
+		return VERIF_CODE::HEAD_INVALID;
 	}
 	
-	if(obj->tail > obj->capacity){
-		return verif_code::tail_invalid;
+	if(obj->capacity <= obj->size){
+		return VERIF_CODE::CORRUPTED_SIZE;
 	}
 
-	if(obj->head >= obj->capacity){
-		return verif_code::head_invalid;
-	}
-
-	if(obj->meta.birth.func_name == NULL ||
-	   obj->meta.birth.file_name == NULL){
-		return verif_code::corrupted_meta;
-	}
-
-	// TODO: magic number ??
 	if(obj->nodes[0].next != 0 || obj->nodes[0].prev != 0){
-		return verif_code::invalid_first_elem;
+		return VERIF_CODE::INVALID_FIRST_ELEM;
 	}
 
-	/*
 	#if DEBUG_MODE == LINEAR_CHECKS
 
+	char* visited = (char*)calloc(obj->capacity, sizeof(char));
 
-	// TODO: запилить линейную проверку
-	
-	for(uint32_t n_elem = 1; n_elem < obj->capacity; n_elem++){
+	if(obj->size > 0){
+		for(int n_node = obj->head; n_node != 0; visited[n_node] = 1, n_node = obj->nodes[n_node].next){
 
-		if(obj->nodes[n_elem].next)
+			int next_ind = obj->nodes[n_node].next;
+
+			if(next_ind >= obj->capacity){
+				return VERIF_CODE::CORRUPTED_LINK;
+			}
+
+			if(visited[n_node]){
+				return VERIF_CODE::CORRUPTED_INJECTIVITY;
+			}
+
+			if(obj->nodes[n_node].prev == -1){
+				return VERIF_CODE::INDICATION_TO_EMPTY_ELEM;
+			}
+
+			if(next_ind != 0){
+				if(n_node != obj->nodes[next_ind].prev){
+					return VERIF_CODE::CORRUPTED_LINK;
+				}
+			}
+		}
 	}
 
-	#endif
-	*/
+	for(int n_node = 1; n_node < obj->capacity; n_node++){
 
-	return verif_code::ok;
+		if(visited[n_node]) continue;
+
+		int next_ind = obj->nodes[n_node].next;
+		
+		if(next_ind >= obj->capacity){
+			return VERIF_CODE::CORRUPTED_LINK;
+		}
+
+		if(visited[next_ind]){
+			return VERIF_CODE::INVALID_FREE_ELEM_POINTER;
+		}
+	}
+	#endif
+
+	return VERIF_CODE::OK;
 }
 
 void list_dump(list* obj, meta_info *meta, META_PARAMS){
@@ -108,46 +527,52 @@ void list_dump(list* obj, meta_info *meta, META_PARAMS){
 	assert(obj  != NULL);
 	assert(meta != NULL);
 
-	to_log("Dump called from function %s(%d)<br>"
+	to_log("<br><br>Dump called from function %s(%d)<br>"
 		   "That was called from file %s, function %s(%d)<br>"
 		   "[#] list %s<%s><br>"
 		   "<br>"
-		   "function_birth_name: %s<br>"
-		   "file_birth_name:     %s<br>"
-		   "n_line_birth_name:   %u<br>"
-		   "function_death_name: %s<br>"
-		   "file_death_name:     %s<br>"
-		   "n_line_death_name:   %u<br>"
-		   "<br>"
-		   "capacity  = %u<br>"
-		   "size      = %u<br>"
-		   "head      = %u<br>"
-		   "tail      = %u<br>"
+		   "capacity  = %d<br>"
+		   "size      = %d<br>"
+		   "head      = %d<br>"
+		   "tail 	  = %d<br>"
+		   "head_free = %d<br>"
+		   "tail_free = %d<br>"
 		   "is_sorted = %d<br>"
-		   "<br>"
-		   "data = [",
+		   "<br>",
 		   func_name, n_line, meta->file_name, meta->func_name, meta->n_line,
-		   obj->meta.name, T_name,
-		   obj->meta.birth.func_name, obj->meta.birth.file_name, obj->meta.birth.n_line,
-		   obj->meta.death.func_name, obj->meta.death.file_name, obj->meta.death.n_line,
-		   obj->capacity, obj->size, obj->head, obj->tail, obj->is_sorted);
+		   meta->obj_name, T_name,
+		   obj->capacity, obj->size, obj->head, obj->tail, obj->head_free, obj->tail_free, obj->is_sorted);
 
-	dump_list_data(obj);
+	dump_nodes(obj);
 
-	to_log("]<br><br>");
-
+	to_log("<br><br>");
 	print_list(obj);
-
+	to_log("<br><br>");
+	
 	return;
 }
 
-void dump_list_data(list* obj){
+void dump_nodes(list* obj){
 	
 	assert(obj != NULL);
 
-	for(int iter = obj->head; iter != obj->tail; iter = obj->nodes[iter].next){
-		to_log("%d, ", obj->nodes[iter].val);
+	to_log("data = [");
+	for(int n_node = 0; n_node < obj->capacity; n_node++){
+		to_log("%5d, ", obj->nodes[n_node].val);
 	}
+	to_log("]<br>");
+
+	to_log("next = [");
+	for(int n_node = 0; n_node < obj->capacity; n_node++){
+		to_log("%5d, ", obj->nodes[n_node].next);
+	}
+	to_log("]<br>");
+
+	to_log("prev = [");
+	for(int n_node = 0; n_node < obj->capacity; n_node++){
+		to_log("%5d, ", obj->nodes[n_node].prev);
+	}
+	to_log("]<br>");
 
 	return;
 }
@@ -156,16 +581,20 @@ void print_list(list* obj){
 
 	assert(obj != NULL);
 
-	FILE* tmp_file = fopen(GVIZ_TMP_NAME, "w");
+	FILE* tmp_file = fopen(GVIZ_DOT_NAME, "w");
 
 	fprintf(tmp_file, "digraph structs {\n"
 			          "rankdir=LR;\n");
 
-	// fix node node node node
-	for(int n_node = 0; n_node < obj->capacity; n_node++){
+	if(obj->capacity > 0){
+		fprintf(tmp_file, "0 [style=\"filled\", fillcolor=\"#BB8FCE\", shape=record,label=\"  <main0> 0 | %d | { %d | %d }\"];\n", 
+			    obj->nodes[0].val, obj->nodes[0].prev, obj->nodes[0].next);
+	}
+	for(int n_node = 1; n_node < obj->capacity; n_node++){
 
-		fprintf(tmp_file, "%d [shape=record,label=\"  <main%d> %d | <prev%d> prev | <next%d> next\"];\n", 
-			    n_node, n_node, n_node, n_node, n_node);
+		fprintf(tmp_file, "%d [style=\"filled\", fillcolor=\"%s\", shape=record,label=\"  <main%d> %d | %d | { %d | %d }\"];\n", 
+			    n_node, (obj->nodes[n_node].prev == FREE_INDICATION_VAL) ? "#AED6F1" : "#82E0AA",
+			    n_node, n_node, obj->nodes[n_node].val, obj->nodes[n_node].prev, obj->nodes[n_node].next);
 	}
 
 	fprintf(tmp_file, "{\n"
@@ -173,22 +602,50 @@ void print_list(list* obj){
     				  "edge[style=invis];\n");
 
 	for(int n_node = 0; n_node < obj->capacity - 1; n_node++){
-
 		fprintf(tmp_file, "%d->", n_node);
 	}
 
-	fprintf(tmp_file, "%d;\n}\n}\n", obj->capacity - 1);
+	fprintf(tmp_file, "%d;\n}\n\n", obj->capacity - 1);
 
-	// TODO: запилить связи
+	for(int n_node = obj->head; obj->nodes[n_node].next != 0; n_node = obj->nodes[n_node].next){
+		fprintf(tmp_file, "%d->%d;\n", n_node, obj->nodes[n_node].next);
+	}
 
+	for(int n_node = obj->head_free; obj->nodes[n_node].next != 0; n_node = obj->nodes[n_node].next){
+		fprintf(tmp_file, "%d->%d[color=\"blue\"];\n", n_node, obj->nodes[n_node].next);
+	}
+
+	fprintf(tmp_file, "\n}\n");
 	fclose(tmp_file);
 
-	// TODO: пофиксить это
+	char* cmd_str = (char*)calloc(CMD_STR_SIZE, sizeof(char));
 
-	WinExec("dot -Tsvg iopv020vc_zxpq_.dot -o ttmp.svg", 0);
+	// TODO: придумать как исправить этот костыль
+	sprintf(cmd_str, "dot -Tsvg %s -o %s", GVIZ_DOT_NAME, GVIZ_HTM_NAME);
+	WinExec(cmd_str, 0);
+	Sleep(80);
+	// КОСТЫЛЬ
 
-	to_log("<br>"
-		   "<img src=%s> <br>", "ttmp.svg");
+    int buflen = get_file_size(GVIZ_HTM_NAME);
+
+	tmp_file = fopen(GVIZ_HTM_NAME, "r");
+ 	
+	char buffer[buflen];
+
+	fread(buffer, sizeof(char), buflen, tmp_file);
+
+	fclose(tmp_file);
+	to_log(buffer);
 
 	return;
+}
+
+int get_file_size(const char* name){
+	
+	assert(name != NULL);
+
+	struct stat st;
+    stat(name, &st);
+
+    return st.st_size;	
 }
